24a69b96143bfa2ca1c0632683d53623221ccc4b
Tyrique Daniel <37724661+TyriqueDaniel@users.noreply.github.com>
2021-07-30 11:54:53 -0600

    Moved Product List Component

--- src/components/ProductItems/index.tsx
+++ src/components/ProductItems/index.tsx
@@ -1,61 +1,66 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import styles from './styles'
 const ProductItem = () => {
-    return (
-        <View style={styles.root}>
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleancode.jpg',
-          }}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title} numberOfLines={3}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            {/* Ratings */}
-          </Text>
+  return (
+    <View style={styles.root}>
+      <Image
+        style={styles.image}
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleancode.jpg',
+        }}
+      />
+      <View style={styles.rightContainer}>
+        <Text style={styles.title} numberOfLines={3}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit.
           {/* Ratings */}
-          <View style={styles.ratingscontainer}>
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star-half-full"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star-o"
-              size={18}
-              color={'#e47911'}
-            />
-            <Text>13,902</Text>
-          </View>
-          <Text style={styles.price}>
-            {' '}
-            from $30.59 <Text style={styles.oldPrice}>$16.91</Text>
-          </Text>
+        </Text>
+        {/* Ratings */}
+        <View style={styles.ratingscontainer}>
+          <FontAwesome
+            style={styles.star}
+            name="star"
+            size={18}
+            color={'#e47911'}
+          />
+          <FontAwesome
+            style={styles.star}
+            name="star"
+            size={18}
+            color={'#e47911'}
+          />
+          <FontAwesome
+            style={styles.star}
+            name="star"
+            size={18}
+            color={'#e47911'}
+          />
+          <FontAwesome
+            style={styles.star}
+            name="star-half-full"
+            size={18}
+            color={'#e47911'}
+          />
+          <FontAwesome
+            style={styles.star}
+            name="star-o"
+            size={18}
+            color={'#e47911'}
+          />
+          <Text>13,902</Text>
         </View>
+        <Text style={styles.price}>
+          {' '}
+          from $30.59 <Text style={styles.oldPrice}>$16.91</Text>
+        </Text>
       </View>
-    )
-}
+    </View>
+  );
+};
+
+
+
+
 
 export default ProductItem;
--- src/screens/HomeScreen/Index.tsx
+++ src/screens/HomeScreen/Index.tsx
@@ -1,62 +1,12 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { StyleSheet, View } from 'react-native';
+import ProductItem from '../../components/ProductItems';
+
 const HomeScreen = () => {
   return (
     <View style={styles.page}>
       {/* Renver Product Component */}
-      <View style={styles.root}>
-        <Image
-          style={styles.image}
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/products/cleancode.jpg',
-          }}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title} numberOfLines={3}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            {/* Ratings */}
-          </Text>
-          {/* Ratings */}
-          <View style={styles.ratingscontainer}>
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star-half-full"
-              size={18}
-              color={'#e47911'}
-            />
-            <FontAwesome
-              style={styles.star}
-              name="star-o"
-              size={18}
-              color={'#e47911'}
-            />
-            <Text>13,902</Text>
-          </View>
-          <Text style={styles.price}>
-            {' '}
-            from $30.59 <Text style={styles.oldPrice}>$16.91</Text>
-          </Text>
-        </View>
-      </View>
+      <ProductItem />
     </View>
   );
 };
@@ -66,44 +16,7 @@ const styles = StyleSheet.create({
     width: '100%',
     padding: 10,
   },
-  root: {
-    flexDirection: 'row',
-    margin: 10,
-    borderWidth: 1,
-    borderColor: '#d1d1d1',
-    borderRadius: 10,
-    backgroundColor: '#fff',
-    width: '100%',
-  },
-  image: {
-    flex: 2,
-    height: 150,
-    resizeMode: 'contain',
-  },
-  rightContainer: {
-    padding: 10,
-    flex: 3,
-  },
-  title: {
-    fontSize: 18,
-  },
-  price: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  oldPrice:{
-  fontSize: 12,
-  fontWeight: 'normal',
-    textDecorationLine: 'line-through'
-  },
-  ratingscontainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 5,
-  },
-  star: {
-    margin: 2,
-  },
+  
 });
 
 export default HomeScreen;
